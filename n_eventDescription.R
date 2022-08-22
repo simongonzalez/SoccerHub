5980382bb01f15d810547769ee8432d756b13615
Simon Gonzalez <simon.gonzalez@anu.edu.au>
2022-08-22 22:35:01 +1000

    Delete n_eventDescription.R

--- n_eventDescription.R
+++
@@ -1,9 +0,0 @@
-output$eventdescriptiontable <-  renderFormattable({
-  
-  formattable(eventsDescription, list(
-    event = formatter("span", 
-                      style = x ~ formattable::style(font.weight = "bold")),
-    description = formatter("span", 
-                            style = x ~ formattable::style(font.weight = "italic"))
-  ))
-})
